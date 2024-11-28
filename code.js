28e5c0a0577c6554c64ad8343fb9598941660a2e
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-11-27 22:07:39 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,40 @@
 function permutationSort(a) {
     var count = 0;
-    return 0;
+
+    if (a.length == 0 || a.length == 1) {
+        return count;
+    }
+
+    // function to check if array is sorted
+    function sortCheck(array) {
+        for (let i = 1; i < array.length; i++) {
+            // if at any point two adjacent elements are not in the correct order, array is not sorted
+            if (array[i] < array[i - 1]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    function permute(n) {
+        if (sortCheck(a)) {
+            return;
+        }
+
+        for (let i = 0; i < n - 1; i++) {
+            permute(n - 1);
+
+            if (n % 2 == 0) {
+                [a[i], a[n - 1]] = [a[n - 1], a[i]];
+            }
+            else {
+                [a[0], a[n - 1]] = [a[n - 1], a[0]];
+            }
+        }
+
+        permute(n - 1);
+    }
+
+    permute(a.length)
+    return count;
 }
